3ec4ee47d3995545e899a9e1298e84b38eafef50
nek0meshi <nekmeshi@gmail.com>
2021-02-13 16:03:45 +0900

    - 不要なソースを削除

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <Board />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Board from './components/Board.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Board
   }
 }
 </script>
---
+++ frontend/src/components/Board.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    ban
+  </div>
+</template>
